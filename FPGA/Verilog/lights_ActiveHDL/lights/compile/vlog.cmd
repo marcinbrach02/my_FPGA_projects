6b612cbe672c4344d2493160f3bed27e64e487f3
marcinbrach02 <46870206+marcinbrach02@users.noreply.github.com>
2019-08-11 22:59:36 +0200

    Added street lights on the walkway project.

---
+++ FPGA/Verilog/lights_ActiveHDL/lights/compile/vlog.cmd
@@ -0,0 +1,1 @@
+-o c:/My_Designs/lights/lights/compile -lc c:/My_Designs/lights/library.cfg -fl c:/My_Designs/lights/lights/projlib.cfg -pli "C:/Aldec/Active-HDL-Student-Edition/bin/systf.dll" -epr c:/My_Designs/lights/lights/compile/lights.epr -O2 -sve -msg 5 -v2k -work lights c:/My_Designs/lights/lights/src/lights_testbench.v 
---
+++ FPGA/Verilog/lights_ActiveHDL/lights/src/lights_testbench.v
@@ -0,0 +1,37 @@
+`default_nettype none
+`timescale 1 ns / 1 ns
+
+  
+module testbench();
+	
+reg R, C, AR, AY, AG, K, PR, PG;	   
+
+
+lights ligInst ( .RST(R), .CLOCK(C), .KEY(K), .P_R(PR), .P_G(PG), .A_R(AR), .A_Y(AY), .A_G(AG));
+
+		   
+
+initial begin
+		R=0;
+	#20 R=1;
+end
+
+
+initial begin
+		K=0;
+	#30 K=1;
+	#20 K=0;	
+end
+
+
+initial begin
+	   C=0;
+end
+
+
+always #2 C = ~C;
+
+
+
+endmodule	
+
