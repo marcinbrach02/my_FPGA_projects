f46b2771c73dbe50d470191c032ccf46e282213d
marcinbrach02 <46870206+marcinbrach02@users.noreply.github.com>
2019-08-20 20:35:31 +0200

    Created project in Diamond 3.10.

---
+++ FPGA/Verilog/microSD/microSD.v
@@ -0,0 +1,17 @@
+module microSD(
+	input wire clk,
+	input wire rst,
+	input wire cs,
+	input wire [47:0] command 
+	
+);
+
+
+//case do obs³ugi rozkazu do karty poprzez wys³anie komendy?
+
+
+
+
+
+
+endmodule
